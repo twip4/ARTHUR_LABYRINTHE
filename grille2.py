a868f60ef12ce22951b0a51c9f4d6daab9a4e315
twip4 <paulbaudinot@gmail.com>
2022-05-12 00:10:54 +0200

    FONCTIONNEL

--- grille2.py
+++ grille2.py
@@ -5,71 +5,53 @@ import random
 g = ouvrirFenetreGrille(80,16,12)
 
 
-maxV = 11
-maxH = 15
-
-Matrice = []
-
-for x in range(maxV) :
-    M = [0 for y in range(maxH)]
-    Matrice.append(M)
+Matrice = [[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1], [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1], [1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1], [1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1], [1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1], [1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1], [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], [1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1], [1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1], [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1], [1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1]]
 
 print(Matrice)
 
-def Maze(Mat,MAXV,MAXH) :
-    H = random.choice(MAXH)
-    V = random.choice(MAXV)
-    for x in range(MAXV):
-        if x != V :
-            Mat[H] = 1
-    print(Mat)
+listpos = []
 
+for x in range(len(Matrice)) :
+    for y in range(len(Matrice[x])) :
+        if Matrice[x][y] == 1 :
+            listpos.append((x,y))
 
+print(listpos)
 
-
-Maze(Matrice,maxV,maxH)
-
-
-#on place des murs
-for i,j in [Matrice] :
+#on place des mur
+for i,j in listpos :
     g.changerCarre(i,j,"black")
 
+posInit = (0,1)
 
-def exploration(i,j,oldi=None,oldj=None,F=None):
-    """Démarre l'exploration de la case de coordonnées (i,j)
-    Cette case est censée être valide et inexplorée (grise). L'exploration consiste en :
-    - changer la case en bleu pour marquer le fait qu'elle est exklorée
-    - déplacer pacman sur cette case
-    - tester chacune des 4 cases voisines, et si cette case est valide et inexplorée, on appelle la fonction exploration dessus
-    - quand une de ces explorations est terminée, on peut remettre pacman en i,j 
-    - en dernière amélioration, on peut placer une flèche entre cette case et la case voisine si une exploration est lancée,
-    et supprimer la flèche quand de la case voisine est est terminée
-    """
-    delay = 0.000005
-    g.changerCarre(i,j,"blue")
-    if oldi != None and oldj != None:
-       F = g.dessinerFleche(oldi,oldj,i,j)
-    g.placer_pacman(i,j)
-    time.sleep(delay)
-    L = [[1,0],[-1,0],[0,1],[0,-1]]
-    random.shuffle(L)
-    for x in L :
-        posX,posY = i+x[0],j+x[1]
-        if g.case_valide(posX,posY) and g.getCouleur(posX,posY) != "blue":
-            if F != None :
-                exploration(posX,posY,i,j,F)
-            exploration(posX,posY,i,j)
-    g.placer_pacman(i,j)
-    if F != None :
-        g.supprimer(F)
-    time.sleep(delay)
+g.placer_pacman(posInit[0],posInit[1])
+g.changerCarre(15,10,"blue")
 
 
-g.attendreClic()
-#exploration(7,5)
+running = True
 
+while running :
+    n = g.attendreTouche()
+    if n == "Up" :
+        if g.case_valide(posInit[0],posInit[1]-1) :
+            posInit = (posInit[0],posInit[1]-1)
+    
+    if n == "Down" :
+        if g.case_valide(posInit[0],posInit[1]+1) :
+            posInit = (posInit[0],posInit[1]+1)
 
-#fin du programme
-g.attendreClic()
+    if n == "Right" :
+        if g.case_valide(posInit[0]+1,posInit[1]) :
+            posInit = (posInit[0]+1,posInit[1])
+    
+    if n == "Left" :
+        if g.case_valide(posInit[0]-1,posInit[1]) :
+            posInit = (posInit[0]-1,posInit[1])
+
+    g.placer_pacman(posInit[0],posInit[1])
+
+    if g.getCouleur(posInit[0],posInit[1]) == "blue" :
+        running = False
+        print("you win !!!!!!")
+
 g.fermerFenetre()
-
--- main.py
+++ main.py
@@ -37,6 +37,29 @@ def Printmat(mat):
     print()
 
 
-
-
 Maze(Matrice,0,maxV,0,maxH)
+
+def valid(maze,x,y):
+  if (x>=0 and x<len(maze) and y>=0 and y<len(maze[0]) and maze[x][y]==1):
+    return True
+  else:
+    return False
+def walk(maze,x,y):
+  if(x==0 and y==0):
+    print("successful!")
+    return True
+  if valid(maze,x,y):
+    # print(x,y)
+    maze[x][y]=2 
+    if not walk(maze,x-1,y):
+      maze[x][y]=1
+    elif not walk(maze,x,y-1):
+      maze[x][y]=1
+    elif not walk(maze,x+1,y):
+      maze[x][y]=1
+    elif not walk(maze,x,y+1):
+      maze[x][y]=1
+    else:
+      return False 
+  return True
+walk(maze,3,3)
